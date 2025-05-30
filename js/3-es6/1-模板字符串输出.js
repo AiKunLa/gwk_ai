3f5441ae3611d0d5292c05e388ee2a908d4161e8
AiKunLa <3535648608@qq.com>
2025-05-30 21:12:33 +0800

    作用域链-闭包-模板字符串-map

---
+++ js/3-es6/1-模板字符串输出.js
@@ -0,0 +1,8 @@
+const name = "John";
+const age = 30;
+const increment = `My dog ${name} is ${age} years old.`;
+
+console.log(increment); // My dog John is 30 years old.
+function getGreeting(name) {
+    return `Hello, ${name}!`;
+}

dd86880797a1a08b3f6ea66922ed0b874bdbe937
AiKunLa <3535648608@qq.com>
2026-03-02 22:19:53 +0800

    区间合并

---
+++ interview/手写题/算法/2-区间合并.js
@@ -0,0 +1,11 @@
+function mearge(arr) {
+    if (arr.length <= 1) return arr
+    arr.sort((a,b) => a[0] - b[0]) // 按照start排序，从小到大
+
+    const result = [arr[0]]
+
+    for (let i = 1; i  < arr.length ; i++) {
+        
+    }
+
+}

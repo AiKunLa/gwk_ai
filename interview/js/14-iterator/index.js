9780c3d1189af2d28b911a2cdf12e1f0c98e3f29
AiKunLa <3535648608@qq.com>
2026-03-05 15:22:50 +0800

    docs(interview): 补充 TypeScript 交叉类型与联合类型的示例
    添加关于组合 BaseProps、ClickableProps 和 DraggableProps 的 ComplexButtonProps 示例，并说明如何为联合类型添加公共字段（如 requestId）。

--- interview/js/14-iterator/index.js
+++ interview/js/14-iterator/index.js
@@ -8,7 +8,7 @@ map2.set('b', 2)
 console.log(typeof myArr[Symbol.iterator]) // "function"
 console.log(typeof map2[Symbol.iterator]) // "function"
 
-const arrIter2 = myArr[Symbol.iterator]() // 获取数组迭代器
+const arrIter2 = myArr[Symbol.iterator]() // 获取数组迭代器：加()表示调用该函数，返回真正的迭代器对象
 console.log(arrIter2.next()) // { value: 1, done: false }
 
 for (const item of map2) {
--- interview/ts/1-type_interface/5-ex.ts
+++ interview/ts/1-type_interface/5-ex.ts
@@ -26,3 +26,35 @@ interface UserRequest extends ApiReuest {
     }
 }
 
+
+// 基础 Props
+type BaseProps = {
+    className?: string;
+    style?: React.CSSProperties;
+};
+
+// 可点击功能
+type ClickableProps = {
+    onClick: () => void;
+    disabled?: boolean;
+};
+
+// 可拖拽功能
+type DraggableProps = {
+    onDragStart: () => void;
+};
+
+// 组合：一个组件同时具备 基础+点击+拖拽
+type ComplexButtonProps = BaseProps & ClickableProps & DraggableProps;
+
+// 甚至可以组合联合类型
+type Status = 'loading' | 'success';
+type LoadingState = { status: 'loading', progress: number };
+type SuccessState = { status: 'success', data: any };
+
+// 交叉类型在这里很难用，通常用 联合类型 (|)
+// 但如果是给每个状态都加上通用 ID：
+// | 是联合类型运算符，表示“或”：取值可以是 LoadingState 或 SuccessState 中的任意一种
+type StateWithId = (LoadingState | SuccessState) & { requestId: string };
+// 结果：(LoadingState & {requestId}) | (SuccessState & {requestId})
+// 这种复杂的类型运算只有 type & 能做到。

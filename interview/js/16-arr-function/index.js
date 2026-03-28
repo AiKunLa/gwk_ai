491aa9bd7f4075ca41ceb55089eaccb02a61914c
AiKunLa <3535648608@qq.com>
2026-03-28 09:40:39 +0800

    feat ：盒模型

--- interview/js/16-arr-function/index.js
+++ interview/js/16-arr-function/index.js
@@ -2,3 +2,12 @@ Array.prototype.isArray = function (arr) {
     if (arr === null || typeof arr === undefined) return false
     return Object.prototype.toString.call(arr) === '[Object Array]'
 }
+
+const obj = {
+    a: 1,
+    say: () => {
+        console.log(this.a)
+    }
+}
+
+obj.say()
